9df168e0dcbc6a1c313eef2a1c3ef39cff7ffc97
Adam Hupp <adam@hupp.org>
2021-10-29 10:39:29 -0700

    lots of stuff, it works now

--- unit_literals/__main__.py
+++ unit_literals/__main__.py
@@ -4,20 +4,19 @@ from pprint import pprint
 if __name__ == "__main__":
   tests = [
     # "1",
-    # "1 * meter",
-    # "1 meter/s**2",
-    # "1 meter",
-    # "1 meter/s^2",
-    # "1 meter/s * 1 second",
-    # "2**4 meters",
-    """
+    "1 meter",
+    "1 meter/s**2",
+    "1 meter",
+    "1 meter/s^2",
+    "1 meter/s * 1 second",
+    "2**4 meters",
+      """
 if foo > 1:
   bar = 5 meters / s
-
-    """,
-    "x = (5 kg) pounds",
+     """,
+      "x = (5 kg) pounds",
     # TODO
-    # "x meters",
+    "x meters",
   ]
   for i in tests:
     print(i)
